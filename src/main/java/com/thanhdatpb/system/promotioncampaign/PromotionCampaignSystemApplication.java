b558db72008c9f9ee6fb350adaf47515bd98d305
Thanh Dat <tthanhdat.pb@gmail.com>
2024-05-26 23:04:00 +0700

    build project

---
+++ src/main/java/com/thanhdatpb/system/promotioncampaign/PromotionCampaignSystemApplication.java
@@ -0,0 +1,13 @@
+package com.thanhdatpb.system.promotioncampaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionCampaignSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionCampaignSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thanhdatpb/system/promotioncampaign/PromotionCampaignSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhdatpb.system.promotioncampaign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionCampaignSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
