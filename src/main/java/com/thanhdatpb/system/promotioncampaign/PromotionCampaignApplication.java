98b5c7137d031759a5734cefaba20a700f307328
Thanh Dat <tthanhdat.pb@gmail.com>
2024-05-27 23:42:40 +0700

    create API and setting

--- src/main/java/com/thanhdatpb/system/promotioncampaign/PromotionCampaignApplication.java
+++ src/main/java/com/thanhdatpb/system/promotioncampaign/PromotionCampaignApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PromotionCampaignSystemApplication {
+public class PromotionCampaignApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PromotionCampaignSystemApplication.class, args);
+		SpringApplication.run(PromotionCampaignApplication.class, args);
 	}
-
 }
